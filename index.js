1218802bbac996de61ac1c8371d8285275ca0fa2
liubgithub <471071362@qq.com>
2017-08-06 17:40:55 +0800

    fix index.js and package.json

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import * as maptalks from 'maptalks';
+import rbush from 'rbush';
 
 maptalks.Map.include({
     startSnapTo(options) {
@@ -6,8 +7,11 @@ maptalks.Map.include({
       if(Array.isArray(geometries)) {
         const len = geometries.length;
         for(let i = 0;i < len; i++) {
-           if (geometries[i] instanceof maptalks.Geometry) {
+           if (geometries[i] instanceof maptalks.Geometry) {//Geometry Object
                const geo = geometries[i].toGeoJSON();
+            }
+            if (geometries[i].type && geometries[i].type == 'Feature') {//geoGeoJSON Object
+               const geo = geometries[i].geometry;
             }
         }
       }
