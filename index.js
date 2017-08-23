eba8327e01788977ab0fb91b4d802996260d9865
liub <471071362@qq.com>
2017-08-23 16:10:58 +0800

    add some method in index.js

--- index.js
+++ index.js
@@ -2,23 +2,31 @@ import * as maptalks from 'maptalks';
 import rbush from 'geojson-rbush';
 
 const options = {
-    'mode': 'point',
+    'mode': 'line',
     'tolerance':10,
     'symbol':{
         'markerType': 'ellipse',
-        'markerFill': 'rgb(10,20,200)',
-        'markerFillOpacity': 1,
+        'markerFill': '#0f89f5',
         'markerLineColor': '#fff',
-        'markerLineWidth': 1,
+        'markerLineWidth': 2,
         'markerLineOpacity': 1,
-        'markerWidth': 20,
-        'markerHeight': 20,
-        'markerDx': 0,
-        'markerDy': 0
+        'markerWidth': 15,
+        'markerHeight': 15
     }
 };
 
-
+/**
+ * Takes an array of LinearRings and optionally an {@link Object} with properties and returns a {@link Polygon} feature.
+ *
+ * @name polygon
+ * @param {Array<Array<Array<number>>>} coordinates an array of LinearRings
+ * @param {Object} [properties={}] an Object of key-value pairs to add as properties
+ * @param {Array<number>} [bbox] BBox [west, south, east, north]
+ * @param {string|number} [id] Identifier
+ * @returns {Feature<Polygon>} a Polygon feature
+ * @throws {Error} throw an error if a LinearRing of the polygon has too few positions
+ * or if a LinearRing of the Polygon does not have matching Positions at the beginning & end.
+ **/
 export class SnapTool extends maptalks.Class {
     constructor(options) {
         super(options);
@@ -48,6 +56,8 @@ export class SnapTool extends maptalks.Class {
         const map = this.getMap();
         if (this.allGeometries) {
             this._registerEvents(map, callback);
+        } else {
+            throw new Error('you should set geometries which are snapped to firstly!');
         }
     }
 
@@ -58,7 +68,23 @@ export class SnapTool extends maptalks.Class {
 
     setGeometries(geometries) {
         geometries = (geometries instanceof Array) ? geometries : [geometries];
-        this.allGeometries = this._compositLine(geometries);
+        this.allGeometries = this._compositGeometries(geometries);
+    }
+
+    setLayer(layer) {
+        if (layer instanceof maptalks.VectorLayer) {
+            const geometries = layer.getGeometries();
+            this.allGeometries = this._compositGeometries(geometries);
+            layer.on('addgeo', function (e) {
+                this._addGeometries(e.geometries);
+            }, this);
+        }
+    }
+
+    _addGeometries(geometries) {
+        geometries = (geometries instanceof Array) ? geometries : [geometries];
+        const addGeometries = this._compositGeometries(geometries);
+        this.allGeometries = this.allGeometries.concat(addGeometries);
     }
 
     _prepareGeometries(coordinate) {
@@ -70,23 +96,24 @@ export class SnapTool extends maptalks.Class {
                 'features':allGeoInGeojson
             });
             this.inspectExtent = this._createInspectExtent(coordinate);
+            //the search result's structure should be remembered
             const availGeometries = this.tree.search(this.inspectExtent);
             return availGeometries;
         }
         return null;
     }
 
-    _compositLine(geometries) {
+    _compositGeometries(geometries) {
         let geos = [];
         geometries.forEach(function (geo) {
             switch (geo.getType()) {
             case 'Point':
+                geo.toGeoJSON().properties = {};
+                geos.push();
                 break;
-            case 'Polyline':
-                geos = geos.concat(this._parserGeometries(geo, 1));
-                break;
+            case 'LineString':
             case 'Polygon':
-                geos = geos.concat(this._parserGeometries(geo, 2));
+                geos = geos.concat(this._parserGeometries(geo, 1));
                 break;
             default:
                 break;
@@ -99,7 +126,7 @@ export class SnapTool extends maptalks.Class {
     _parserGeometries(geo, _len) {
         const coordinates = geo.getCoordinates();
         let geos = [];
-        //分两种情况，一种是单条线的， 另一种是多条线
+        //two cases,one is single geometry,and another is multi geometries
         if (coordinates[0].x && coordinates[0].y) {
             const _lines = this._createLine(coordinates, _len, geo);
             geos = geos.concat(_lines);
@@ -145,6 +172,7 @@ export class SnapTool extends maptalks.Class {
     }
     _registerEvents(map, callback) {
         this._mousemove = function (e) {
+            this.mousePoint = e.coordinate;
             if (!this._marker) {
                 this._marker = new maptalks.Marker(e.coordinate, {
                     'symbol' : this.options['symbol']
@@ -152,45 +180,37 @@ export class SnapTool extends maptalks.Class {
             } else {
                 this._marker.setCoordinates(e.coordinate);
             }
-            const availLines = this._findGeometry(e.coordinate);
-            if (availLines.features.length > 0) {
-                if (callback) callback(availLines);
-                console.log(availLines.features.length);
-                const snapPoint = this._getSnapPoint(availLines);
-                this._marker.setCoordinates([snapPoint.x, snapPoint.y]);
+            const availGeometries = this._findGeometry(e.coordinate);
+            if (availGeometries.features.length > 0) {
+                if (callback) callback(availGeometries);
+                console.log(availGeometries.features.length);
+                const snapPoint = this._getSnapPoint(availGeometries);
+                if (snapPoint) {
+                    this._marker.setCoordinates([snapPoint.x, snapPoint.y]);
+                }
             }
         };
         map.on('mousemove', this._mousemove, this);
     }
 
-    _setDistance(lines) {
-        for (let i = 0; i < lines.length; i++) {
-            const line = lines[i];
-            const distance = this._distToPolyline(this._marker.getCenter(), line);
-            line.properties.distance = distance;
-            const lineEquation = this._setEquation(line);
-            const VerticalLine = this._setVertiEquation(lineEquation.B / lineEquation.A, this._marker.getCenter());
-            const insectPoint = this._solveEquation(lineEquation, VerticalLine);
-            line.properties.insectPoint = insectPoint;
+    _setDistance(geos) {
+        for (let i = 0; i < geos.length; i++) {
+            const geo = geos[i];
+            if (geo.geometry.type === 'LineString') {
+                const distance = this._distToPolyline(this.mousePoint, geo);
+                geo.properties.distance = distance;
+            } else if (geo.geometry.type === 'Point') {
+                const distance = this._distToPoint(this.mousePoint, geo);
+                geo.properties.distance = distance;
+            }
         }
-        return lines;
+        return geos;
     }
 
-    _findNearestLine(lines) {
-        lines = this._setDistance(lines);
-        lines = lines.sort(this._compare(lines, 'distance'));
-        return lines[0];
-    }
-
-    _findLines(features) {
-        const lines = this._compositLine(features);
-        let tree = rbush();
-        tree.load({
-            'type': 'FeatureCollection',
-            'features':lines
-        });
-        const availLines = tree.search(this.inspectExtent);
-        return availLines;
+    _findNearestGeometries(geos) {
+        geos = this._setDistance(geos);
+        geos = geos.sort(this._compare(geos, 'distance'));
+        return geos[0];
     }
 
     _findGeometry(coordinate) {
@@ -198,20 +218,28 @@ export class SnapTool extends maptalks.Class {
         return availGeimetries;
     }
 
-    _getSnapPoint(availLines) {
-        const _nearestLine = this._findNearestLine(availLines.features);
-        const nearestLine = this._setEquation(_nearestLine);
-        const verticalLine = this._setVertiEquation(nearestLine.A / nearestLine.B, this._marker.getCenter());
-        const snapPoint = this._solveEquation(nearestLine, verticalLine);
-        return snapPoint;
+    _getSnapPoint(availGeometries) {
+        const _nearestGeometry = this._findNearestGeometries(availGeometries.features);
+        if (!this._validDistance(_nearestGeometry.properties.distance)) {
+            return null;
+        }
+        //when point, return itself
+        if (_nearestGeometry.geometry.type === 'Point') {
+            return _nearestGeometry;
+        } else if (_nearestGeometry.geometry.type === 'LineString') {
+            //when line,return the vertical insect point
+            const nearestLine = this._setEquation(_nearestGeometry);
+            const k = nearestLine.B / nearestLine.A;
+            if (k) {
+                const verticalLine = this._setVertiEquation(k, this.mousePoint);
+                const snapPoint = this._solveEquation(nearestLine, verticalLine);
+                return snapPoint;
+            }
+        }
+        return null;
     }
 
-    _snap() {
-        const mousePoint = this._marker.toGeoJSON();
-        return mousePoint;
-    }
-
-    _getAllGeometries() {
+    /*_getAllGeometries() {
         let _allGeometries = [];
         if (this._snapLayer) {
             _allGeometries = this._snapLayer.getGeometries();
@@ -225,7 +253,7 @@ export class SnapTool extends maptalks.Class {
             });
         }
         return _allGeometries;
-    }
+    }*/
     ///
     _toGeoJSON(geometries) {
         const _snapGeometries = [];
@@ -246,6 +274,23 @@ export class SnapTool extends maptalks.Class {
         const C = equation.C;
         const distance = Math.abs((A * point.x + B * point.y + C) / Math.sqrt(Math.pow(A, 2) + Math.pow(B, 2)));
         return distance;
+    }
+
+    _validDistance(distance) {
+        const map = this.getMap();
+        const resolution = map.getResolution();
+        const tolerance = this.options['tolerance'];
+        if (distance / resolution > tolerance) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    _distToPoint(mousePoint, toPoint) {
+        const from = [mousePoint.x, mousePoint.y];
+        const to = toPoint.geometry.coordinates;
+        return Math.sqrt(Math.pow(from[0] - to[0], 2) + Math.pow(from[1] - to[1], 2));
     }
     //create a line's equation
     _setEquation(line) {
